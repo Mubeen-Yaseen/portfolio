2baa0eee656d39c761498e92eea0638a4bdd9533
Mubeen Yaeen <mubeenyaseen7257287@gmail.com>
2024-07-01 20:02:40 +0500

    Personal Potfolio

---
+++ Script.js
@@ -0,0 +1,11 @@
+
+
+
+
+// const togglebars = document.querySelector('.bars');
+// const Bars_icon = document.querySelector('.bars i'); 
+// const Dropdown = document.querySelector('.dropdown');
+
+// togglebars.onclick = function () { 
+//     Dropdown.classList.toggle('open'); 
+// };
